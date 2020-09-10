0ac679c19f2f7267c7f2e446f4c366305db0d02c
Jason Long <jlong5795@gmail.com>
2020-09-10 15:40:37 -0500

    setting up new deployment

--- knexfile.js
+++ knexfile.js
@@ -31,27 +31,38 @@ module.exports = {
       tableName: 'knex_migrations',
     },
   },
-
   production: {
-    client: 'postgresql',
-    // connection: process.env.DATABASE_URL,
-    connection: {
-      user: process.env.DB_USER,
-      password: process.env.DB_PW,
-      database: process.env.DB_DB,
-      host: process.env.DB_HOST,
-      port: 5432,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './data/migrations',
-      tableName: 'knex_migrations',
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds',
+      directory: "./data/seeds/",
     },
+    useNullAsDefault: true
   },
+
+  // production: {
+  //   client: 'postgresql',
+  //   // connection: process.env.DATABASE_URL,
+  //   connection: {
+  //     user: process.env.DB_USER,
+  //     password: process.env.DB_PW,
+  //     database: process.env.DB_DB,
+  //     host: process.env.DB_HOST,
+  //     port: 5432,
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     directory: './data/migrations',
+  //     tableName: 'knex_migrations',
+  //   },
+  //   seeds: {
+  //     directory: './data/seeds',
+  //   },
+  // },
 };
